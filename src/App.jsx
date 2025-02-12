81c34fccff7c8f1d2d500d7a74cd990b7cfffee0
Mihai Tartara <mihai.tirtara@gmail.com>
2025-02-12 16:12:49 +0100

    Set up main wrapper componet with background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Header from './components/Header'
 import Board from './components/Board'
+import background from './assets/background.jpg'
 function App() {
- 
+  console.log(background)
 
   return (
-    <div class='m-0 w-full'>
+    <div className="min-h-screen w-full bg-[url('/src/assets/background.jpg')] bg-fixed bg-cover bg-center ">
     <Header/>
     <Board/>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import headerImage from '../assets/header.png'
 
 export default function Header(){
     return(
-        <header class="w-full bg-white">
+        <header class="w-full ">
             <div class="container mx-auto justify-center flex">
                 <img src={headerImage} alt="Header Image" class="w-64 mx-auto  object-contain"></img>
            </div>
